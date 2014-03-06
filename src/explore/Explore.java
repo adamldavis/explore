68cf343e15bc6e1ebd905ea0f58e83f3fff03a1f
adamd <adam.dav@gmail.com>
2014-03-06 10:09:12 -0500

    Asked for name and email; inputAndDo is now more generic

--- src/explore/Explore.java
+++ src/explore/Explore.java
@@ -1,4 +1,3 @@
-
 package explore;
 
 import java.io.IOException;
@@ -8,6 +7,7 @@ import static java.lang.System.err;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Optional;
 
 import java.util.Scanner;
@@ -23,21 +23,24 @@ import java.util.stream.Stream;
 public class Explore {
 
     static final Scanner scan = new Scanner(in);
-    
+
     static final Logger logger = Logger.getLogger(Explore.class.getName());
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         out.println("Hello, welcome to Explore version 0.0.1");
         out.println();
-        inputAndDo(() -> "What is the name of your project?",
-                Explore::inputString, Explore::createProject);
+        inputAndDo(() -> new String[]{
+            "What is the name of your project?",
+            "What is your name?",
+            "What is your email?"},
+                Explore::inputStrings, Explore::createProject);
     }
 
-    private static <T> void inputAndDo(Supplier<String> messagef,
-            Function<String,T> inputf, Function<T,Optional<Exception>> dof) {
+    private static <S, T> void inputAndDo(Supplier<S> messagef,
+            Function<S, T> inputf, Function<T, Optional<Exception>> dof) {
         Stream.generate(messagef)
                 .map(inputf)
                 .map(dof)
@@ -45,34 +48,46 @@ public class Explore {
                 .limit(1)
                 .forEach(x -> out.println("Done"));
     }
-    
-    public static Optional<Exception> createProject(String name) {
+
+    public static Optional<Exception> createProject(String[] array) {
         try {
-            Path p  = Paths.get(name);
+            final String name = array[0];
+            final String user = array[1];
+            final String email = array[2];
+            Path p = Paths.get(name);
             Files.createDirectory(p);
+            Path readme = Paths.get(name, "README");
+            Files.createFile(readme);
+            Files.write(readme, Arrays.asList("Project: " + name,
+                    "Author: " + user, "Email: " + email));
         } catch (IOException ex) {
             out.println("Please try again");
             return Optional.of(ex);
         }
         return Optional.empty();
     }
-    
+
     public static int inputInt(String str) {
         out.println(str);
         return scan.nextInt();
     }
-    
+
+    public static String[] inputStrings(String[] str) {
+        return Arrays.stream(str).map(Explore::inputString)
+                .toArray((n) -> new String[n]);
+    }
+
     public static String inputString(String str) {
         out.println(str);
-        return scan.next();        
+        return scan.next();
     }
-    
+
     public static final void err(String s) {
         err.println(s);
     }
-    
+
     public static final void err(Exception e) {
         e.printStackTrace(err);
     }
-    
+
 }
